369e4fbc156fbbb6799415f27ff7c0042a5bb4bf
Parth Patel <31615065+Parth910@users.noreply.github.com>
2025-05-06 10:50:30 +0530

    Update Home.tsx
    updated headline

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -24,7 +24,7 @@ const Home: React.FC = () => {
           </div>
           <div className="flex items-center justify-center mt-8 md:mt-16 text-center">
             <p className="text-base sm:text-lg md:text-lg lg:text-xl text-center max-w-2xl">
-              Passionate about building efficient, high-performance software. With 4+ years of experience, I specialize in backend development, database optimization, and system scalability.
+             Experienced Full Stack Engineer with 4+ years building high-performance web applications from end to end. Specializes in React (JS/TypeScript) for pixel-perfect UIs and Node.js for scalable backend services, including REST/microservice APIs and database optimizations (SQL/NoSQL). Passionate about clean architecture and system reliability.
             </p>
           </div>
         </div>
